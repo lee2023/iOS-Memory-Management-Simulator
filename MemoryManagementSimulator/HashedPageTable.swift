169f399bdd86c822aaaec53b4790045e6c4725c0
lee2023 <rosemary.espinal20@gmail.com>
2016-11-28 02:04:03 -0500

    Added CPU, HashedPageTable, and PhysicalMemory classes. Modified all other classes.

--- MemoryManagementSimulator/HashedPageTable.swift
+++ MemoryManagementSimulator/HashedPageTable.swift
@@ -10,10 +10,9 @@ import Foundation
 
 class PageTable {
     //Page table (Hash Table) [ page# : frame# ]
-    var pageTable = [Int: Int]()
+    var pageTable = [Int: (Int,Int)]()
 
     private func hashFunction(pageNumber: Int) -> Int {
-        //var frameNumber: Int
         var frameNumber = 0
         for (key) in pageTable {
             if key.key == pageNumber {
@@ -25,17 +24,19 @@ class PageTable {
         return frameNumber
     }
     
+    func addProcessAddressToTable(pageNumber: Int) {
+        let frame = hashFunction(pageNumber: pageNumber)
+        pageTable.updateValue(frame, forKey: pageNumber)
+    }
+    
     func printPageTable() {
         if pageTable.isEmpty {
             print("The page table is empty! No process address spaces have been added!")
         }
         
         for (key, value) in pageTable {
-            print("[\(key) : \(value)]")
+            print("[ Page Number: \(key) : Frame Number: \(value)]")
         }
     }
     
-    func addProcessAddressToTable(processVirtualAddress: Int) {
-        
-    }
 }
---
+++ MemoryManagementSimulator/PhysicalMemory.swift
@@ -0,0 +1,9 @@
+//
+//  PhysicalMemory.swift
+//  MemoryManagementSimulator
+//
+//  Created by Rosemary Espinal on 11/27/16.
+//  Copyright © 2016 Espinal Designs, LLC. All rights reserved.
+//
+
+import Foundation
